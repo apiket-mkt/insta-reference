bf5ab5ef9b1f0228e6b9edbd56d7014f1e31db58
apiket-mkt <apiket-mkt@example.com>
2026-04-03 17:03:21 +0900

    Final link: connect Google Sheets CSV

--- src/data.js
+++ src/data.js
@@ -4,4 +4,4 @@ export const GLOBAL_PASSWORD = 'noir_hair';
  * 구글 스프레드시트 CSV 내보내기 주소입니다.
  * [파일] -> [공유] -> [웹에 게시] -> [쉼표로 구분된 값(.csv)]를 선택하여 생성된 URL을 여기에 붙여넣으세요.
  */
-export const SHEETS_CSV_URL = 'PASTE_YOUR_SHEET_URL_HERE';
+export const SHEETS_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT1I9xAhF7Qoz4d4FduPLze_lFSBvNzfpqITfoUDqoALFZygi1qWtVBU5U_gRQ0p6wRDdHHbbZ5VQlM/pub?output=csv';
